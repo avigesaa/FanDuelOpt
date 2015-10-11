52aebec6b2a69931dd8116c4c166fb39b3d9214a
Andrew Vigesaa <avigesaa@gmail.com>
2015-10-11 18:09:55 -0400

    Move gen_lp.py into its own directory.

--- fanduel-lpsolve/gen_lp.py
+++ fanduel-lpsolve/gen_lp.py
@@ -11,6 +11,7 @@ Example usage:
 from collections import namedtuple
 from argparse import ArgumentParser
 from csv import DictReader
+import random
 
 PlayerDatum = namedtuple('PlayerDatum', ['name', 'position', 'salary', 'projection'])
 
@@ -39,7 +40,7 @@ def load_player_data(filename):
     return player_data
 
 
-def write_lp_file(player_data, salary_cap):
+def write_lp_file(player_data, salary_cap, proj_jitter):
     """
     Write an lp_solve .lp file to stdout.
 
@@ -47,7 +48,7 @@ def write_lp_file(player_data, salary_cap):
     :param salary_cap: The maximum salary amount for an individual team.
     """
     def get_var_name(player):
-        return '%s_%s' % (player.position, player.name.replace('-', '').replace('.', '').replace(' ', ''))
+        return '%s_%s' % (player.position, filter(str.isalpha, player.name))
 
     # Get a list of all the players:
     all_players = []
@@ -56,7 +57,7 @@ def write_lp_file(player_data, salary_cap):
             all_players.append(p)
 
     # Write the objective function:
-    print 'max: %s;' % ' + '.join(['%s %s' % (p.projection, get_var_name(p)) for p in all_players])
+    print 'max: %s;' % ' + '.join(['%s %s' % (p.projection * random.uniform(1.0-proj_jitter, 1.0+proj_jitter), get_var_name(p)) for p in all_players])
 
     # Write the position constraints:
     print 'qb_lim: %s = 1;' % ' + '.join([get_var_name(p) for p in player_data['QB']])
@@ -67,7 +68,7 @@ def write_lp_file(player_data, salary_cap):
     print 'd_lim: %s = 1;' % ' + '.join([get_var_name(p) for p in player_data['D']])
 
     # Write the salary constraint:
-    print 'sal_lim: %s <= %s;' % (' + '.join(['%s %s' % (p.salary, get_var_name(p)) for p in all_players]), args.salary_cap)
+    print 'sal_lim: %s <= %s;' % (' + '.join(['%s %s' % (p.salary, get_var_name(p)) for p in all_players]), salary_cap)
 
     # Write the binary variables:
     print 'bin %s;' % ' '.join(get_var_name(p) for p in all_players)
@@ -79,6 +80,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('player_data_file', help='The .CSV file containing player data')
     arg_parser.add_argument('--salary-cap', type=int, default=60000, help='The team salary cap restriction')
     arg_parser.add_argument('--min-player-proj', type=float, default=2.718, help='Minimum point projection required to consider a player.')
+    arg_parser.add_argument('--proj-jitter', type=float, default=0.0, help='Adds a random adjustment to player projections')
     args = arg_parser.parse_args()
 
     # Load player data from the input file:
@@ -89,4 +91,4 @@ if __name__ == '__main__':
         player_data[position] = filter(lambda p: p.projection >= args.min_player_proj, player_data[position])
 
     # Write the .lp file:
-    write_lp_file(player_data, args.salary_cap)
+    write_lp_file(player_data, args.salary_cap, args.proj_jitter)
